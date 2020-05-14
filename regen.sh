3c5c60eaeab43c6ac006e6ad149538ec4edcd07f
Zhang Zengbo <zengbo.zhang@gmail.com>
2020-05-14 11:58:39 +0800

    make protobuf versions differs

--- regen.sh
+++ regen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-protoc --cpp_out=cpp messages.proto
-protoc --nanopb_out=c messages.proto 
+PROTOBUF_VERSION=`protoc --version | awk '{print $2}'`
+mkdir -p cpp/${PROTOBUF_VERSION}
+protoc --cpp_out=cpp/${PROTOBUF_VERSION} messages.proto
+protoc --nanopb_out=c messages.proto
 node types-gen-c.js
 node errcode-gen-c.js
